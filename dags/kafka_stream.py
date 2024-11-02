608404f72f7500e365238ed54b0d1bb7f2907733
andresbasilea <andresbasilealvarez@gmail.com>
2024-11-02 17:14:42 -0600

    kafka stream automatic requests for 60 seconds. Added cassandra and spark to compose

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -29,19 +29,37 @@ def format_data(res):
 
 def stream_data():
     import json
-    res = get_data()
-    res = format_data(res)
-    print(json.dumps(res, indent=3))
+    from kafka import KafkaProducer
+    import time
+    import logging
+
+    producer = KafkaProducer(bootstrap_servers=['broker:9092'], max_block_ms=5000)
+    curr_time = time.time()
+    #print(json.dumps(res, indent=3))
+
+    while True:
+        if time.time() > curr_time + 60:
+            break
+        try:
+            res = get_data()
+            res = format_data(res)
+            producer.send('users_created', json.dumps(res).encode('utf-8'))
+        
+        except Exception as e:
+            logging.error(f'Error: {e}')
+            continue
 
 
-# with DAG('automation_user', 
-#          default_args=default_args,
-#          schedule_interval='@daily',
-#          catchup=False) as dag:
+
+
+with DAG('automation_user', 
+         default_args=default_args,
+         schedule_interval='@daily',
+         catchup=False) as dag:
     
-#     streaming_task = PythonOperator(
-#         task_id = 'stream_data_from_api',
-#         python_callable=stream_data
-#     )
+    streaming_task = PythonOperator(
+        task_id = 'stream_data_from_api',
+        python_callable=stream_data
+    )
 
 stream_data()
